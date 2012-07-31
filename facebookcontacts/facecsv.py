68374bce9f16a749dd4f52b4d2f8115973155460
JP Viljoen <froztbyte@froztbyte.net>
2012-07-31 17:23:30 +0200

    some changes based on review recommendations

--- facebookcontacts/facecsv.py
+++ facebookcontacts/facecsv.py
@@ -1,27 +1,35 @@
 #!/usr/bin/python
-""" code by froztbyte, (C) WTFPL 2012 """
+"""
+facebook contact export clobber script
+
+this is hellabad. it assumes names and email addresses are present, nothing
+more than that. then it clobbers things.
+
+code by froztbyte, MIT License 2012
+"""
 import argparse
 from os import getcwd
 
-""" because fuck manual arg parsing forever """
-parser = argparse.ArgumentParser(description="pop out a nicer format for fb export data")
+# because fuck manual arg parsing forever
+parser = argparse.ArgumentParser(
+    description="pop out a nicer format for fb export data")
 parser.add_argument('--input', help='input file', type=None, required=False)
 parser.add_argument('--output', help='output file', type=None, required=False)
 
 args = parser.parse_args()
 
-""" loads appropriate var paths, assumes files in cwd by default """
-if args.input is not None:
-    INPUT = args.input
-else:
+# loads appropriate var paths, assumes files in cwd by default
+if args.input is None:
     INPUT = getcwd() + '/fb.txt'
-if args.input is not None:
-    OUTPUT = args.output
 else:
+    INPUT = args.input
+if args.input is None:
     OUTPUT = getcwd() + '/output.txt'
+else:
+    OUTPUT = args.output
 
-""" read the file, drop it in a list """
-initList = open(INPUT, 'r').readlines()
+#read the file, drop it in a list
+initList = open(INPUT, 'r')
 
 
 def stripsearch(input):
@@ -30,14 +38,17 @@ def stripsearch(input):
     if temp != '':
         return temp
 
-""" the real work """
-""" we make the assumption that we're keeping list order all the way through """
+#the real work
+#we make the assumption that we're keeping list order all the way through
 strippedBlanks = filter(stripsearch, initList)
 for idx, item in enumerate(strippedBlanks):
     strippedBlanks[idx] = item.strip()
 
+# yes, that just happened.
+# this code is on github, after all.
+
 names = {}
-for idx, item in enumerate(strippedBlanks):
+for item in strippedBlanks:
     if '@' not in item:
         name = item
         names[name] = []
@@ -45,11 +56,10 @@ for idx, item in enumerate(strippedBlanks):
         names[name].append(item)
 
 csvLines = []
-for item in names:
-    for address in names[item]:
-        linestruct = "%(name)s,%(surname)s,%(address)s" % {
-            'name': item.split(' ')[0],
-            'surname': item.split(' ', 1)[1],
+for name, addresses in names.iteritems():
+    for address in addresses:
+        linestruct = "%(name)s,%(address)s" % {
+            'name': name,
             'address': address}
         csvLines.append(linestruct + '\n')
 
